7aba5fe33be5c02c3b9eea96795a5f3e7c1dc650
Angelika Jarosz <angelikajarosz@gmail.com>
2016-09-14 14:33:52 -0400

    index template at root url

--- ickly/settings.py
+++ ickly/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ickly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ickly', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
