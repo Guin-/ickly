7bb678f9e910a2dfff2ef0703e2cf9463014b342
Angelika Jarosz <angelikajarosz@gmail.com>
2017-01-17 16:39:03 -0500

    update serializer, viewset handles missing required fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,9 +2,6 @@ from rest_framework import serializers
 
 class BusinessSerializer(serializers.Serializer):
     camis = serializers.CharField()
-    dba = serializers.CharField(required=False)
-    boro = serializers.CharField(required=False)
-    street = serializers.CharField(required=False)
-    building = serializers.CharField(required=False)
-    zipcode = serializers.CharField(required=False)
-    cuisine_description = serializers.CharField(required=False)
+    dba = serializers.CharField(allow_blank=True)
+    address = serializers.CharField(allow_blank=True)
+    cuisine_description = serializers.CharField(allow_blank=True)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets, mixins
 from serializers import BusinessSerializer
-
+from ickly import secret_settings
 from sodapy import Socrata
+
+class Business(object):
+    def __init__(self, camis, dba, cuisine_description, address):
+        self.camis = camis
+        self.dba = dba
+        self.cuisine_description = cuisine_description
+        self.address = address
+
 
 class BusinessesViewSet(mixins.ListModelMixin,
                         viewsets.GenericViewSet):
@@ -9,11 +17,20 @@ class BusinessesViewSet(mixins.ListModelMixin,
     serializer_class = BusinessSerializer
 
     def get_queryset(self):
-        client = Socrata("data.cityofnewyork.us", "z9bHXnSMLOVBFTGrELvWCcCBN")
-        businesses = client.get("9w7m-hzhe", content_type="json",
-                                limit="500000",
+        client = Socrata("data.cityofnewyork.us", secret_settings.APP_TOKEN)
+        businesses_data = client.get("9w7m-hzhe", content_type="json",
+                                limit="100",
                                 select="camis, dba, boro, building, street, zipcode, cuisine_description")
+
         # Get all unique businesses
-        businesses = {v['camis']:v for v in businesses}.values()
+        businesses_data = {v['camis']:v for v in businesses_data}.values()
+
+        businesses = [Business(camis=business.get('camis', ''),
+                                dba=business.get('dba', ''),
+                                cuisine_description=business.get('cuisine_description', ''),
+                                address=business.get('building', '') + ' ' +
+                                        business.get('street', '') + ' ' +
+                                        business.get('boro', '') + ' ' +
+                                        business.get('zipcode', '')) for business in businesses_data]
         return businesses
 
