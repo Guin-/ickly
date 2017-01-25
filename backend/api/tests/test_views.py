d2d291059300d6d1d72aaf7c40e898ce48c7b304
Angelika Jarosz <angelikajarosz@gmail.com>
2017-01-25 16:51:07 -0500

    tests use django test client instead of requests library

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,14 +1,17 @@
 import pytest
 import vcr
-import requests
+from django.test import Client
+
+client = Client()
 
 @vcr.use_cassette()
-def test_api_root():
-    response = requests.get('http://localhost:8000/api/v1/')
+def test_api_root(client):
+    response = client.get('/api/v1/')
     assert response.status_code == 200
-    assert 'businesses' in response.text
 
-@vcr.use_cassette()
-def test_businesses_endpoint():
-    response = requests.get('http://localhost:8000/api/v1/businesses/')
+
+@vcr.use_cassette(record_mode='new_episodes')
+def test_businesses_endpoint(client):
+    response = client.get('/api/v1/businesses/')
     assert response.status_code == 200
+
