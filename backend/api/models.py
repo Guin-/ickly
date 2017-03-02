ef982c5777a3232686765a639cf1755b9a958480
Angelika Jarosz <angelikajarosz@gmail.com>
2017-03-01 20:58:54 -0500

    add inspection model and migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,3 +8,16 @@ class Business(models.Model):
     address = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     cuisine_description = models.CharField(max_length=255)
+
+class Inspection(models.Model):
+    business = models.ForeignKey(Business)
+    record_date = models.DateField()
+    inspection_date = models.DateField()
+    inspection_type = models.CharField(max_length=255)
+    action = models.CharField(max_length=255)
+    violation_code = models.CharField(max_length=255)
+    violation_description = models.TextField()
+    critical_flag = models.CharField(max_length=255)
+    score = models.SmallIntegerField(null=True)
+    grade = models.CharField(max_length=255)
+    grade_date = models.DateField(null=True)
