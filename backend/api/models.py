099ddc8de3bd6b7e7c9f9813d18af1514694ee13
Angelika Jarosz <angelikajarosz@gmail.com>
2017-03-01 19:54:47 -0500

    add business model and migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Business(models.Model):
+    camis = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    cuisine_description = models.CharField(max_length=255)
