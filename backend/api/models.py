355c89208515648a9551f9b623da7b73ed7be7f5
Angelika Jarosz <angelikajarosz@gmail.com>
2017-03-07 13:52:14 -0500

    add changes to business and inspection models

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,21 +3,47 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Business(models.Model):
-    camis = models.CharField(max_length=255)
+    camis = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
     cuisine_description = models.CharField(max_length=255)
 
 class Inspection(models.Model):
+    GRADE_A = 'A'
+    GRADE_B = 'B'
+    GRADE_C = 'C'
+    NOT_YET_GRADED = 'N'
+    GRADE_PENDING_REOPENING = 'P'
+    GRADE_PENDING = 'Z'
+
+    GRADE_CHOICES = (
+        (GRADE_A, 'A'),
+        (GRADE_B, 'B'),
+        (GRADE_C, 'C'),
+        (NOT_YET_GRADED, 'Not Yet Graded'),
+        (GRADE_PENDING_REOPENING, 'Grade Pending issued on re-opening following an initial inspection that resulted in a closure'),
+        (GRADE_PENDING, 'Grade Pending'),
+    )
+
+    CRITICAL = 'Critical'
+    NOT_CRITICAL = 'Not Critical'
+    NOT_APPLICABLE = 'N/A'
+
+    CRITICAL_FLAG_CHOICES = (
+        (CRITICAL, 'Critical'),
+        (NOT_CRITICAL, 'Not Critical'),
+        (NOT_APPLICABLE, 'Not Applicable'),
+    )
+
     business = models.ForeignKey(Business)
     record_date = models.DateField()
     inspection_date = models.DateField()
     inspection_type = models.CharField(max_length=255)
     action = models.CharField(max_length=255)
-    violation_code = models.CharField(max_length=255)
+    violation_code = models.CharField(max_length=3)
     violation_description = models.TextField()
-    critical_flag = models.CharField(max_length=255)
+    critical_flag = models.CharField(max_length=12, choices=CRITICAL_FLAG_CHOICES)
     score = models.SmallIntegerField(null=True)
-    grade = models.CharField(max_length=255)
+    grade = models.CharField(max_length=1, choices=GRADE_CHOICES)
     grade_date = models.DateField(null=True)
