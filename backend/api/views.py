e6cc46091ad3c2999c7494dd60173136db19d27e
Angelika Jarosz <angelikajarosz@gmail.com>
2017-04-21 13:53:54 -0400

    add drf SearchFilter

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, filters
 from serializers import BusinessSerializer
 from models import Business
 
 class BusinessesViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()
     serializer_class = BusinessSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
 
