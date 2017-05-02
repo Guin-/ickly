e1cc6bf899580319b27ac13954aa34cf39cffbcd
Angelika Jarosz <angelikajarosz@gmail.com>
2017-05-02 16:51:30 -0400

    businesses endpoint integration tests

--- backend/ickly/test_settings.py
+++ backend/ickly/test_settings.py
@@ -1,10 +1,3 @@
 from settings import *
 
-DATABASES = {
-     "default": {
-          "ENGINE": "django.db.backends.sqlite3",
-          "NAME": ":memory:",
-          }
-}
-
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
