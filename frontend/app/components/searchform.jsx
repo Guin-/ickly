63a39fb0b6a1730ded018b8442aed7bacb0d51bd
Angelika Jarosz <angelikajarosz@gmail.com>
2017-05-04 17:10:55 -0400

    Typeahead component fetches businesses endpoint data with users search query

--- frontend/app/components/searchform.jsx
+++ frontend/app/components/searchform.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import { AsyncTypeahead }  from 'react-bootstrap-typeahead';
 
-
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         options: []
         };
-
   }
+
+  handleSearch(query) {
+    if (!query) {
+      return;
+    }
+
+    fetch('http://localhost:8000/api/v1/businesses/?search=' + query)
+      .then(resp => resp.json())
+      .then(json => this.setState({options: json.results}))
+  };
+
 
   render() {
     return (
         <div className="container">
-          <AsyncTypeahead
-            labelKey={(option) => option['dba'] + ' ' + option['address']}
-            onSearch={ query => (
-              fetch('http://localhost:8000/api/v1/businesses')
-                .then(resp => resp.json())
-                .then(json => json['results'])
-                .then(json => this.setState({options: json}))
-            )}
-            options={this.state.options}
-          />
+          <div>
+            <AsyncTypeahead
+              placeholder="Search for a restaurant..."
+              labelKey={(option) => option['name'] + ' ' + option['address']}
+              onSearch={this.handleSearch.bind(this)}
+              options={this.state.options}
+            />
+          </div>
         </div>
     );
   }
+
 }
 
 export default SearchForm;
