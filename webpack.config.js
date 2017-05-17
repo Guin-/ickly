f2b158f7abd41f172cefda3ce49a973cc7dbefa1
Angelika Jarosz <angelikajarosz@gmail.com>
2017-05-17 15:15:25 -0400

    remove unnecessary babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-0'],
+          presets: ['react', 'es2015'],
         },
       },
       { test: /\.scss$/,
