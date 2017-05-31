4e311e286d70bf666e182347ea1253582921d7a7
Angelika Jarosz <angelikajarosz@gmail.com>
2017-05-31 16:30:38 -0400

    move logic from searchForm into BusinessSearch container

--- frontend/app/components/searchform.jsx
+++ frontend/app/components/searchform.jsx
@@ -4,36 +4,7 @@ import { AsyncTypeahead }  from 'react-bootstrap-typeahead';
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-        options: [],
-        selected: []
-        };
   }
-
-  handleSearch(query) {
-    if (!query) {
-      return;
-    }
-
-    fetch('/api/v1/businesses/?search=' + query)
-      .then(resp => resp.json())
-      .then(json => this.setState({options: json.results}))
-  };
-
-
-  renderMenuItemChildren(option, props, index) {
-    return (
-      <div key={option.camis} onClick={(e) => this.handleTypeAheadResultClick(option)}>
-        <strong>{option.name}</strong>
-        <p>{option.address}</p>
-      </div>
-    );
-  }
- handleTypeAheadResultClick(option) {
-   fetch('/api/v1/businesses/' + option['camis'])
-    .then(resp => resp.json())
-    .then(json => this.setState({selected: json}))
- }
 
   render() {
     return (
@@ -42,16 +13,10 @@ class SearchForm extends React.Component {
             <AsyncTypeahead
               placeholder="Search for a restaurant..."
               labelKey={(option) => option['name']}
-              onSearch={this.handleSearch.bind(this)}
-              options={this.state.options}
-              renderMenuItemChildren={this.renderMenuItemChildren.bind(this)}
+              onSearch={this.props.handleSearch}
+              options={this.props.options}
+              renderMenuItemChildren={this.props.renderMenuItemChildren}
             />
-          </div>
-          <div>
-            <strong>{this.state.selected.name}</strong>
-            <p>{this.state.selected.address}</p>
-            <p>{this.state.selected.phone}</p>
-            <p>{this.state.selected.cuisine_description}</p>
           </div>
         </div>
     );
--- frontend/app/containers/BusinessSearch.jsx
+++ frontend/app/containers/BusinessSearch.jsx
@@ -1,18 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SearchForm from '../components/searchform';
+import store from '../index';
+import { fetchBusiness, receiveBusinessDetail } from '../actions/';
 
 class BusinessSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchQuery: '',
+      options: []
+    };
+
+    this.handleTypeAheadResultClick = this.handleTypeAheadResultClick.bind(this)
   }
+
+  handleSearch(query) {
+    if (!query) {
+      return;
+    }
+
+    fetch('/api/v1/businesses/?search=' + query)
+      .then(resp => resp.json())
+      .then(json => this.setState({options: json.results, searchQuery: query}))
+      .catch(err => console.error('Business Search: handleSearch failed', err))
+  };
+
+
+  renderMenuItemChildren(option, props, index) {
+    return (
+      <div key={option.camis} onClick={(e) => this.handleTypeAheadResultClick(option)}>
+        <strong>{option.name}</strong>
+        <p>{option.address}</p>
+      </div>
+    );
+  }
+
+  handleTypeAheadResultClick(option) {
+    const { dispatch } = this.props
+    dispatch(fetchBusiness(option));
+  }
+
 
   render() {
     return (
       <div className='container'>
-        <SearchForm />
+        <SearchForm
+          handleSearch={this.handleSearch.bind(this)}
+          options={this.state.options}
+          renderMenuItemChildren={this.renderMenuItemChildren.bind(this)}
+        />
       </div>
     )
   }
 }
 
-export default BusinessSearch;
+function mapStateToProps(state) {
+  const { fetchBusiness, receiveBusinessDetail } = state
+  const { isFetching, item } =  fetchBusiness ||
+    { isFetching: true, item: []}
+  return {
+    item,
+    isFetching,
+  }
+}
+
+
+export default connect()(BusinessSearch)
+
