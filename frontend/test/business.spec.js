affda5c2e921bb7bb1c7fd52e32fca4ed0c3d6a0
Angelika Jarosz <angelikajarosz@gmail.com>
2017-06-19 12:50:52 -0400

    test for business component

--- frontend/test/business.spec.js
+++ frontend/test/business.spec.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Business from '../app/components/business';
 
 describe('<Business />', () => {
   it('should render the name of the selected business', () => {
-    const wrapper = shallow(<Business />)
-    expect(wrapper.find('div')).to.have.length(1)
+    const props = {
+      selectedBusiness: {name: 'restaurant'}
+    }
+    const wrapper = mount(<Business {...props} />)
+    const name = wrapper.find('h1')
+    expect(name.text()).to.equal('restaurant')
   });
 });
