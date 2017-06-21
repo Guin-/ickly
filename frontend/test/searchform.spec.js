318aacb38182be447349d4bf61b3f0f94641efd9
Angelika Jarosz <angelikajarosz@gmail.com>
2017-06-21 13:17:07 -0400

    code clean up

--- frontend/test/searchform.spec.js
+++ frontend/test/searchform.spec.js
@@ -7,16 +7,6 @@ import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 
 describe('<SearchForm />', () => {
   it('should render the typeahead search form', () => {
-/*
-    const props = {
-      options: [
-        { name: 'restaurant', camis: '123456', address: 'location'},
-        { name: 'restaurant2', camis: '223456', address: 'location2'}
-      ],
-      renderBusinessOptions: sinon.spy(),
-      handleSearch: sinon.spy()
-    }
-*/
     const wrapper = shallow(<SearchForm />)
     expect(wrapper.find(AsyncTypeahead)).to.have.length(1);
   });
@@ -26,13 +16,4 @@ describe('<SearchForm />', () => {
     expect(wrapper.props().handleSearch).to.be.defined;
     expect(wrapper.props().renderBusinessOptions).to.be.defined;
   })
-/*
-  it('should call handleSearch if the user inputs a query', () => {
-    const handleSearchSpy = sinon.spy()
-    const wrapper = mount(<SearchForm handleSearch={handleSearchSpy} />)
-    //simulate user input
-    wrapper.find(AsyncTypeahead).simulate('change', {target: {value: 'my query'}})
-    expect(handleSearchSpy.called).to.equal(true)
-  })
-*/
 });
