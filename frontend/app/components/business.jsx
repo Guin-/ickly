18075b46e6045ccb118e4c21966bfcbab1af9462
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-25 14:43:59 -0400

    business detail view

--- frontend/app/components/business.jsx
+++ frontend/app/components/business.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  padding: 1em;
+`;
 
 const Business = ({selectedBusiness}) => (
-  <div>
-    <h1>{selectedBusiness.name}</h1>
-  </div>
+  <Wrapper>
+    <div>
+      <h4>{selectedBusiness.name}</h4>
+      <h5>{selectedBusiness.phone}</h5>
+      <h5>{selectedBusiness.address}</h5>
+      <h5>{selectedBusiness.cuisine_description}</h5>
+    </div>
+  </Wrapper>
 )
 
 Business.propTypes = {
