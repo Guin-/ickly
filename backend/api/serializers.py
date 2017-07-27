d0698a014aa6271ac0bec81859de558421b52604
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-27 12:36:18 -0400

    business inspection edge endpoint

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from models import Business
+from models import Business, Inspection
 
 class BusinessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Business
         fields = ('camis', 'name', 'address', 'phone', 'cuisine_description')
+
+class InspectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inspection
+        fields = ('business', 'record_date', 'inspection_date', 'inspection_type', 'action',
+                  'violation_code', 'violation_description', 'critical_flag', 'score',
+                  'grade', 'grade_date')
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from views import BusinessesViewSet
+from views import BusinessesViewSet, BusinessInspectionEdgeEndpoint
 
 router = routers.DefaultRouter()
 router.register(r'businesses', BusinessesViewSet)
+router.register(r'businesses/(?P<parent_id>\d+)/inspections', BusinessInspectionEdgeEndpoint)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
