8f221abaf82a56e84c2d1ede2cabc1490588d79a
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-28 13:44:21 -0400

    display filter for phone number format

--- frontend/app/components/business.jsx
+++ frontend/app/components/business.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import NumberFormat from 'react-number-format'
 
 const Wrapper = styled.section`
   padding: 1em;
@@ -10,7 +11,7 @@ const Business = ({selectedBusiness}) => (
   <Wrapper>
     <div>
       <h4>{selectedBusiness.name}</h4>
-      <h5>{selectedBusiness.phone}</h5>
+      <NumberFormat value={selectedBusiness.phone} displayType={'text'} format="(###) ###-####"/>
       <h5>{selectedBusiness.address}</h5>
       <h5>{selectedBusiness.cuisine_description}</h5>
     </div>
