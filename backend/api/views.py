88c019d5b9c0c1ce018abb034d5ee41ee554eb43
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-28 14:21:50 -0400

    044 business inspection edge endpoint (#45)
    * business inspection edge endpoint
    
    * business inspection edge endpoint tests

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,22 @@
 from rest_framework import viewsets, filters
-from serializers import BusinessSerializer
-from models import Business
+from serializers import BusinessSerializer, InspectionSerializer
+from models import Business, Inspection
+
+class BaseEdgeViewSet(viewsets.ModelViewSet):
+
+    allowed_methods = ['GET', 'POST', 'PATCH', 'DELETE']
+
+    def __init__(self, *args, **kwargs):
+        super(BaseEdgeViewSet, self).__init__(*args, **kwargs)
+
+    parent_field = ''
+
+    def get_queryset(self):
+        """
+        Returns QuerySet of all items on the parent
+        """
+        parent_id = self.kwargs['parent_id']
+        return self.queryset.filter(**{self.parent_field: parent_id})
 
 class BusinessesViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()
@@ -8,3 +24,7 @@ class BusinessesViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
 
+class BusinessInspectionEdgeEndpoint(BaseEdgeViewSet):
+    queryset = Inspection.objects.all()
+    parent_field = 'business'
+    serializer_class = InspectionSerializer
