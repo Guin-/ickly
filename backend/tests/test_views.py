558a321982eeb6ec3bb5dfec70c991fcd0cf1d76
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-28 12:55:28 -0400

    business inspection edge endpoint tests

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -28,3 +28,16 @@ def test_businesses_endpoint_detail_response(client):
     assert expected_keys == response.data.keys()
     assert len(response.data) == 5
     assert response.data['camis'] == '40394054'
+
+
+@pytest.mark.django_db
+def test_business_inspection_edge_endpoint_response(client):
+    response = client.get('/api/v1/businesses/40394054/inspections/')
+    expected_keys = ['business', 'record_date', 'inspection_date', 'inspection_type',
+                     'action', 'violation_code', 'violation_description', 'critical_flag',
+                     'score', 'grade', 'grade_date']
+    assert response.status_code == 200
+    for d in response.data['results']:
+        assert d.keys() == expected_keys
+        assert d['business'] == '40394054'
+
