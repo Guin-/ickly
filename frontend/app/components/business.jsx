037aae596cfb20b6e7af65d13db452970dd08a04
Angelika Jarosz <angelikajarosz@gmail.com>
2017-07-28 13:46:15 -0400

    business detail view (#43)
    * business detail view
    
    * display filter for phone number format

--- frontend/app/components/business.jsx
+++ frontend/app/components/business.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import NumberFormat from 'react-number-format'
+
+const Wrapper = styled.section`
+  padding: 1em;
+`;
 
 const Business = ({selectedBusiness}) => (
-  <div>
-    <h1>{selectedBusiness.name}</h1>
-  </div>
+  <Wrapper>
+    <div>
+      <h4>{selectedBusiness.name}</h4>
+      <NumberFormat value={selectedBusiness.phone} displayType={'text'} format="(###) ###-####"/>
+      <h5>{selectedBusiness.address}</h5>
+      <h5>{selectedBusiness.cuisine_description}</h5>
+    </div>
+  </Wrapper>
 )
 
 Business.propTypes = {
