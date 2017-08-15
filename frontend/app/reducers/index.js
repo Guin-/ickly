bd4cd133a611b1252d7e937ef282775689e129d4
Angelika Jarosz <angelikajarosz@gmail.com>
2017-08-15 12:14:10 -0400

    047 inspections data in redux store (#48)
    * inspections data in redux store
    
    * remove console log

--- frontend/app/reducers/index.js
+++ frontend/app/reducers/index.js
@@ -1,32 +1,9 @@
-import { BUSINESS_DETAIL_REQUEST, BUSINESS_DETAIL_SUCCESS, BUSINESS_DETAIL_FAILURE, RESET_ERROR} from '../actions/'
+import { combineReducers } from 'redux'
+import businessDetail from './businessDetail'
+import inspectionsList from './inspections'
 
-function businessDetail(state = {
-  isFetching: false,
-  selectedBusiness : {},
-  error: null
-}, action) {
-  switch (action.type) {
-    case BUSINESS_DETAIL_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true
-    })
-    case BUSINESS_DETAIL_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        error: action.error
-    })
-    case BUSINESS_DETAIL_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        selectedBusiness: action.businessDetail
-    })
-    case RESET_ERROR:
-      return Object.assign({}, state, {
-        error: null
-    })
-    default:
-      return state
-  }
-}
+export default combineReducers({
+  business: businessDetail,
+  inspections: inspectionsList
+})
 
-export default businessDetail;
