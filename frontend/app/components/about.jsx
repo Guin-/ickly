6696d133fa9515a87707db46083d886a93bb2cb4
Angelika Jarosz <angelikajarosz@gmail.com>
2017-10-18 15:29:39 -0400

    about component, images for contact

--- frontend/app/components/about.jsx
+++ frontend/app/components/about.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap'
+
 
 class About extends React.Component {
   constructor(props) {
@@ -7,20 +9,21 @@ class About extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 text-center">
-            <h1>About Ickly</h1>
-              <p className="lead">A search interface for NYC Restaurant Inspection Results data</p>
-                <ul className="list-unstyled">
-                  <li>It does some things</li>
-                  <li>Made by a guin</li>
-                </ul>
-          </div>
-        </div>
-      </div>
+      <Grid>
+        <Row>
+          <Col lg={8} lgOffset={2} md={8} mdOffset={2} xs={10} xsOffset={1} className="text-center">
+            <h2>About Ickly</h2>
+              <p>Ickly is a search interface into <a href="https://data.cityofnewyork.us/Health/DOHMH-New-York-City-Restaurant-Inspection-Results/xx67-kt59">NYC Restaurant Inspection Data</a> provided by NYC Open Data.
+                Users can look up restaurants by name and get a list of detailed inspection data including
+                grades, overall scores, and violation descriptions.
+                It was built with Django, Django Rest Framework, React, Redux, and React Bootstrap.
+              </p>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
 
 export default About;
+
