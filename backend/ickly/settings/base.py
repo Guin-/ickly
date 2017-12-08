f78c616f0340937812a7ee9ad39ae1836c82f17c
Angelika Jarosz <angelikajarosz@gmail.com>
2017-12-08 17:42:26 -0500

    move bundles

--- backend/ickly/settings/base.py
+++ backend/ickly/settings/base.py
@@ -46,7 +46,7 @@ WEBPACK_LOADER = {
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../frontend/dist/'),
+    os.path.join(BASE_DIR, '../frontend/output/'),
     os.path.join(BASE_DIR, '../frontend/static/'),
 )
 
