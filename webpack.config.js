3039b20e33817563e77787a9f77a7cdb3aed0311
Angelika Jarosz <angelikajarosz@gmail.com>
2017-12-12 15:09:39 -0500

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,9 @@ module.exports = {
       },
       { test: /\.css$/,
         loaders: ['style', 'css']
+      },
+      { test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'file'
       }
     ]
   },
