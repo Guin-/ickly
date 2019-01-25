b295e58a1d02c1ede1ae868273ec5e15554c75a0
Angelika Jarosz <angelikajarosz@gmail.com>
2019-01-25 10:46:19 -0500

    068 search form styling (#86)
    * search form and intro text
    
    * clean up business component

--- frontend/app/components/business.jsx
+++ frontend/app/components/business.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import NumberFormat from 'react-number-format'
 
-const Wrapper = styled.section`
-  padding: 1em;
-`;
-
 const Business = ({business}) => (
-  <Wrapper>
-    <div>
+    <div className='col-lg-6 offset-lg-3'>
       <h4>{business.name}</h4>
       <h5><NumberFormat value={business.phone} displayType={'text'} format="(###) ###-####"/></h5>
       <h5>{business.address}</h5>
       <h5>{business.cuisine_description}</h5>
     </div>
-  </Wrapper>
 )
 
 Business.propTypes = {
