6642619320c3ab70758e1904cb08e6768e427caf
Angelika Jarosz <angelikajarosz@gmail.com>
2019-02-08 15:28:25 -0500

    inspections list data section

--- frontend/app/components/inspections.jsx
+++ frontend/app/components/inspections.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 class Inspections extends React.Component {
   constructor(props) {
@@ -9,6 +10,8 @@ class Inspections extends React.Component {
 
   render() {
     const columns = [{
+      expander: true
+    }, {
       Header: 'Date',
       accessor:'inspection_date'
     }, {
@@ -24,13 +27,59 @@ class Inspections extends React.Component {
     if (this.props.inspections.length > 0) {
       return(
         <>
-          <p> Inspections list </p>
-          <ReactTable
-            data={inspections}
-            columns={columns}
-            showPagination={false}
-            defaultPageSize={5}
-          />
+          <div className="col-lg-10 offset-lg-1">
+            <ReactTable
+              data={inspections}
+              columns={columns}
+              showPagination={false}
+              pageSize={inspections.length}
+              defaultPageSize={10}
+              SubComponent={ (row) =>{
+                return(
+                  <div>
+                  <ListGroup>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Inspection Type</ListGroupItemHeading>
+                        {row['original']['inspection_type']}
+                      <ListGroupItemText>
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Action</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {row['original']['action']}
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Violation Description</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {row['original']['violation_description'] || 'N/A'}
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Violation Code</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {row['original']['violation_code'] || 'N/A'}
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Critical Flag</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {row['original']['critical_flag']}
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Grade Date</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        {row['original']['grade_date'] || 'N/A'}
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                  </ListGroup>
+                  </div>
+                );
+              }}
+            />
+          </div>
         </>
       )
     }
