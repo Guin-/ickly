09097bf97f3d4c936a5f81f4fd12a78eb3b9350b
Angelika Jarosz <angelikajarosz@gmail.com>
2019-02-26 12:30:38 -0500

    103 routing issue (#105)
    * remove debug mode in prod
    
    * add a catch all server side route

--- backend/ickly/settings/prod.py
+++ backend/ickly/settings/prod.py
@@ -1,7 +1,5 @@
 from .base import *
 
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 WEBPACK_LOADER['DEFAULT'].update({
--- backend/ickly/urls.py
+++ backend/ickly/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include('api.urls', namespace='api'))
+    url(r'^api/v1/', include('api.urls', namespace='api')),
+    url(r'^.*/', TemplateView.as_view(template_name='index.html'))
 ]
