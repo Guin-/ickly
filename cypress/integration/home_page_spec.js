d185bc62f999d10f962b37b05120817d074a8a01
Angelika Jarosz <angelikajarosz@gmail.com>
2019-04-19 14:47:24 -0400

    clean landing page spec

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,21 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
+describe('Clean Home Page', function() {
+  it('successfully loads with navigation, header, and form', function() {
     cy.visit('/')
+
+    cy.get('.hero-wrapper').within((heroWrapper) => {
+      cy.contains('.navbar-custom', 'About')
+      cy.get('.ickly-header')
+    })
+
+    cy.get('form').within((el) => {
+      cy.get('input:first')
+    })
+  })
+
+  it('does not render the inspections component', function() {
+    cy.get('.business-detail')
+    cy.get('[data-test=inspections]').should('not.exist')
   })
 })
+
+
