a58cdb18f9f07883cf92007d569794ea6b992a6d
Angelika Jarosz <angelikajarosz@gmail.com>
2019-05-02 12:28:22 -0400

    131 integration tests (#132)
    * tests form focuses and clears
    
    * update clean landing page spec

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -16,6 +16,43 @@ describe('Clean Home Page', function() {
     cy.get('.business-detail')
     cy.get('[data-test=inspections]').should('not.exist')
   })
+
+  it('has expected state on load', () => {
+    cy.window().its('store').invoke('getState').should('deep.equal', {
+      business: {
+        isFetching: false,
+        selectedBusiness: {},
+        error: null
+      },
+      inspections: {
+        inspections: [],
+        error: null
+      }
+    })
+  })
+
+  it('can navigate between pages', function() {
+    cy.get('.nav-link:first')
+      .click()
+    cy.get('.navbar-brand')
+      .click()
+  })
+
 })
 
+describe('Form User Interactions', function() {
+  it('form focuses and clears on click, displays dropdown', function() {
+    cy.get('form').within((el) => {
+      cy.get('input:first')
+        .click()
+        .type('demi-monde')
+       cy.get('.dropdown-menu').should('exist')
+        .get('li:first')
+       cy.get('input:first')
+        .click()
+        .focused()
+       cy.get('.dropdown-menu').should('not.exist')
+    })
+  })
+})
 
